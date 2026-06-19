f68fdbbb9b6e1dd04a594c2dc81a3978e7e832b1
BenjaminLlanquiman <be.llanquiman@duocuc.cl>
2026-06-18 20:47:05 -0400

    se agrega modifica securityconfig

--- bff-dashboard/src/main/java/com/grupocordillera/bff/config/SecurityConfig.java
+++ bff-dashboard/src/main/java/com/grupocordillera/bff/config/SecurityConfig.java
@@ -21,7 +21,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
